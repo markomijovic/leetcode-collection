8fe604603e3bafb139ae26b52150b3e4104b27d6
Marko Mijovic <mijovicmarko11@gmail.com>
2021-07-01 21:58:15 -0600

    medium

---
+++ _2021July Daily Challenge/01_GrayCode.py
@@ -0,0 +1,19 @@
+"""
+An n-bit gray code sequence is a sequence of 2n integers where:
+
+    Every integer is in the inclusive range [0, 2n - 1],
+    The first integer is 0,
+    An integer appears no more than once in the sequence,
+    The binary representation of every pair of adjacent integers differs by exactly one bit, and
+    The binary representation of the first and last integers differs by exactly one bit.
+
+Given an integer n, return any valid n-bit gray code sequence.
+"""
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = []
+        for i in range(1 << n):
+            graycode = i ^ (i >> 1)
+            res.append(graycode)
+        return res
