f518f70dc1d13e2f05f30497a96b5c3d1409633d
Marko Mijovic <mijovicmarko11@gmail.com>
2022-01-02 21:44:25 -0700

    easy

---
+++ General/Array/Find Judge Town.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        if n==1:
+            return 1
+        
+        score = [0] * (n+1)
+        
+        for a, b in trust:
+            score[a] -= 1
+            score[b] += 1
+        
+        for i, val in enumerate(score[1:n+1], 1):
+            if val == n-1:
+                return i
+        return -1
